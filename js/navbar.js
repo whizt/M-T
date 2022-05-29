362147bdb2da0a533662410693ce222e5a9ca9e7
Hansen Andersen <hansen.sutikno@binus.ac.id>
2022-05-30 03:20:34 +0700

    progress 5
    navbar update
    - hansen

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,32 @@
-// function open() {
-//   document.getElementById('hamburger').className = 'hamburg';
-// }
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction() {
-    var x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropbtn").classList.toggle("rotate");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropclick = document.getElementsByClassName("dropbtn");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      var rotateDropbtn = dropclick[i];
+      // $(this).css("transform","rotate(90deg)");
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        rotateDropbtn.classList.remove('rotate');
+      }
     }
   }
+}
+
+$( ".dropbtn" ).onclick(function(event) {
+  if (  $( this ).css( "transform" ) == 'none' ){
+      $(this).css("transform","rotate(90deg)");
+  } else {
+      $(this).css("transform","" );
+  }
+});
